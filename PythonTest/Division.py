c80967d2242184957d567e9cf5789e2c970c3798
Chandran <nachiyar.s.chandran@accenture.com>
2016-01-22 10:18:29 +0530

    git commit

---
+++ PythonTest/Division.py
@@ -0,0 +1,14 @@
+from fit.ColumnFixture import ColumnFixture
+
+class Division(ColumnFixture):
+  _typeDict={
+      "numerator": "Float",
+      "denominator": "Float",
+      "quotient": "Float",
+      "quotient.charBounds": "99",
+  }
+  numerator = 0.0
+  denominator = 0.0
+
+  def quotient(self):
+      return self.numerator / self.denominator
